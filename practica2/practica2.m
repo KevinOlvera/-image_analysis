54f07c01f6863753b60c49310d8a4fb3a2c1db00
Kevin Olvera <kevin.jesus.olvera@gmail.com>
2021-03-03 16:47:37 -0600

    añadi subplot para los histogramas

--- practica2/practica2.m
+++ practica2/practica2.m
@@ -16,12 +16,12 @@ imshow(imagen_1)
 title('Imagen Original')
 
 % --- seccionamos la imagen en sus componentes de color
-img_roja = imagen(:,:,1);
-img_verde = imagen(:,:,2);
-img_azul = imagen(:,:,3);
+img_roja = imagen_1(:,:,1);
+img_verde = imagen_1(:,:,2);
+img_azul = imagen_1(:,:,3);
 
 % --- obtenemos el tamaño de la imagen
-[alto, ancho, color] = size(imagen);
+[alto, ancho, color] = size(imagen_1);
 
 
 % --- menu para ingresar los datos
@@ -37,7 +37,29 @@ while validar
     % --- ...
     
     
-    
+    % --- Se muestran en pantalla los histogramas
+    figure(2)
+    % --- Histogramas originales
+    subplot(2,3,1)
+    histogram(img_roja)
+    title('Canal Rojo')
+    subplot(2,3,2)
+    histogram(img_verde)
+    title('Canal Verde')
+    subplot(2,3,3)
+    histogram(img_azul)
+    title('Canal Azul')
+    % --- Histogramas ampliados
+    % --- TODO: cambiar la entrada de estos histogramas por los calculados
+    subplot(2,3,4)
+    histogram(img_roja)
+    title('Canal Rojo Ampliado')
+    subplot(2,3,5)
+    histogram(img_verde)
+    title('Canal Verde Ampliado')
+    subplot(2,3,6)
+    histogram(img_azul)
+    title('Canal Azul Ampliado')
     
     % --- Calcular de nuevo
     respuesta = input('¿Deseas calcular con otros valores? S/N','s');
